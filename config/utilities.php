ae9a1db8fcbe2ce46085a281b28e4333d1ce09b9
Michael Jennings <michaeljennings@gmlconsulting.co.uk>
2018-07-17 10:54:08 +0100

    Update config

--- config/utilities.php
+++ config/utilities.php
@@ -2,8 +2,19 @@
 
 return [
 
+    /*
+     |---------------------------------------------------------------------
+     | Domains
+     |---------------------------------------------------------------------
+     |
+     | Here you can define the domains you want to be available in the
+     | domain builder. The key is the value you will request from the
+     | domain builder, and the value is the url you want to build to.
+     |
+    */
+
     'domains' => [
-        // 'app'
+        // 'app' => 'http://app.example.com'
     ],
 
 ];
